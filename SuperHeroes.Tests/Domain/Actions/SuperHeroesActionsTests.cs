2471bf2cdad998b526f7caefc1d7bd4061ab4744
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2023-11-22 21:31:14 -0300

    Finalizando primeiro teste

--- SuperHeroes.Tests/Domain/Actions/SuperHeroesActionsTests.cs
+++ SuperHeroes.Tests/Domain/Actions/SuperHeroesActionsTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SuperHeroes.Domain.Actions;
 using SuperHeroes.Domain.Interfaces.Actions;
 using SuperHeroes.Domain.Interfaces.Repositories;
 using SuperHeroes.Domain.Models;
 using SuperHeroes.Domain.VOs.SuperHeroes;
+using SuperHeroes.Infra.CrossCutting.ServiceLocator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,19 +45,31 @@ namespace SuperHeroes.Tests.Domain.Actions
 
             SuperHeroes.Domain.Models.SuperHeroes superHeroes = new()
             {
-                Name = "Teste",
-                HeroName = "Teste",
-                BirthDate = DateTime.Now,
-                Height = 1,
-                Weight = 1
+                Name = createSuperHeroVO.Name,
+                HeroName = createSuperHeroVO.HeroName,
+                BirthDate = createSuperHeroVO.BirthDate,
+                Height = createSuperHeroVO.Height,
+                Weight = createSuperHeroVO.Weight,
             };
 
-            _superHeroesRepository.Setup(x => x.Add(It.Equals(superHeroes))).ReturnsAsync(1);
+            _superHeroesRepository.Setup(x => x.Add(It.Is<SuperHeroes.Domain.Models.SuperHeroes>(x =>
+                x.Name == superHeroes.Name &&
+                x.HeroName == superHeroes.HeroName &&
+                x.BirthDate == superHeroes.BirthDate &&
+                x.Height == superHeroes.Height &&
+                x.Weight == superHeroes.Weight
+            ))).ReturnsAsync(1);
 
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<ISuperHeroesRepository>(_superHeroesRepository.Object);
 
+            ServiceLocator.SetProvider(services.BuildServiceProvider());
 
             SuperHeroesActions superHeroesActions = new();
 
+            int result = superHeroesActions.Create(createSuperHeroVO).Result;
+
+            Assert.AreEqual(1, result);
         }
     }
 }
